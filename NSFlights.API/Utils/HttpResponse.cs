d16a4696048ce1c089e8b1892c01cba587117180
Jorge Loaiza <jlloaiza04@gmail.com>
2024-03-21 12:53:14 -0500

    feat(Flights): add controller(Flight)/endpoint(Journey)

---
+++ NSFlights.API/Utils/HttpResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Sales.API.Utils
+{
+    public class HttpResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+        public string Message { get; set; }
+    }
+}
--- NSFlights.IOC/Dependency.cs
+++ NSFlights.IOC/Dependency.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Sales.BLL.Services.Interfaces;
+using Sales.BLL.Services;
 
 namespace Sales.IOC
 {
     public static class Dependency
     {
-        public static void InjectDependencies(this IServiceCollection services)
+        public static void InjectDependencies(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddMemoryCache();
+
+            services.AddScoped<IJourneyService, JourneyService>(options => new JourneyService(configuration.GetConnectionString("JourneyHost")));
         }
     }
 }
