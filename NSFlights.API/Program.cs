4240dfe347f7f59713c388385f3d0bbf8fa657b7
Jorge Loaiza <jlloaiza04@gmail.com>
2024-03-21 12:54:42 -0500

    fix(build): add arg Configuration to main dependencyInjection

--- NSFlights.API/Program.cs
+++ NSFlights.API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.InjectDependencies();
+builder.Services.InjectDependencies(builder.Configuration);
 
 var app = builder.Build();
 
