49e76e315eb2dd05776b7cedc804513cab023b08
Jorge Loaiza <jlloaiza04@gmail.com>
2024-03-21 11:38:43 -0500

    add: ApiClient Interface/Flights API

---
+++ NSFlights.DAL/ApiService/Interfaces/IApiClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales.DAL.ApiService.Interfaces
+{
+    public interface IApiClient<T> where T : class
+    {
+        public Task<T> GetAsync();
+        public Task<T> PostAsync(T data);
+        public Task<T> PutAsync(T data);
+        public Task<T> DeleteAsync(T data);
+    }
+}
---
+++ NSFlights.DAL/ApiService/JourneyApi.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Sales.DAL.ApiService.Interfaces;
+using Sales.Model;
+
+namespace Sales.DAL.ApiService
+{
+    public class JourneyApi : IApiClient<JourneyResponse>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _url;
+
+        public JourneyApi(string url)
+        {
+            _httpClient = new HttpClient();
+            _url = url;
+        }
+
+        public async Task<JourneyResponse> GetAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(_url);
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+            Journey[] arr = JsonConvert.DeserializeObject<Journey[]>(json)!;
+
+            return new JourneyResponse([.. arr]);
+        }
+
+        public Task<JourneyResponse> PostAsync(JourneyResponse data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JourneyResponse> PutAsync(JourneyResponse data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JourneyResponse> DeleteAsync(JourneyResponse data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
