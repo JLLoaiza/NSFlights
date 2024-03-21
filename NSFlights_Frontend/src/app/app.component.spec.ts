9d888854e0d4200234adb25d87c256f34b4a070a
Jorge Loaiza <jlloaiza04@gmail.com>
2024-03-21 13:24:30 -0500

    refactor: rearrange frontend folder

--- NSFlights_Frontend/src/app/app.component.spec.ts
+++ NSFlights_Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-NSFlights'`, () => {
+  it(`should have as title 'NSFlights_Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-NSFlights');
+    expect(app.title).toEqual('NSFlights_Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-NSFlights app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('NSFlights_Frontend app is running!');
   });
 });
