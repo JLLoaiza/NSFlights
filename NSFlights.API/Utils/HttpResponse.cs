8a3bac8e971d2d7eff5e839ece6756496d5ca4be
Jorge Loaiza <jlloaiza04@gmail.com>
2024-03-22 02:58:14 -0500

    refactor: delete API folder

--- NSFlights.API/Utils/HttpResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sales.API.Utils
-{
-    public class HttpResponse<T>
-    {
-        public bool IsSuccess { get; set; }
-        public T Value { get; set; }
-        public string Message { get; set; }
-    }
-}
