25748d7687b09776c9409ba61ce3b908339edb68
Jorge Loaiza <jlloaiza04@gmail.com>
2024-03-22 06:38:52 -0500

    add(backend): cors policy

--- NSFlights_Backend/NSFlights.API/Program.cs
+++ NSFlights_Backend/NSFlights.API/Program.cs
@@ -12,6 +12,14 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.InjectDependencies(builder.Configuration);
 
+builder.Services.AddCors(options => options.AddPolicy(
+    name: "FrontendUI",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }
+));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("FrontendUI");
 
 app.UseHttpsRedirection();
 
